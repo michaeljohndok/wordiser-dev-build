4d3ab177bcdca5405da015fbae6686bb431bf9bc
guoquan mai <nacheal@gmail.com>
2018-05-19 02:58:04 +0800

    uploaded...

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('./workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
 // Force development builds
 // workbox.setConfig({ debug: true });
